82a31e30d2bfcfd4f6a0a83bf40a000b9aac770f
mahdi <mahdi.mohseni0000@gmail.com>
2024-08-19 18:24:35 +0330

    models befor migration

---
+++ combat_exam/registration/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class User (models.Model):
+    first_name = models.CharField(max_length= 30, blank=False)
+    familly_name = models.CharField(max_length=40, blank=False)
+    email = models.EmailField(max_length=50)
+    user_name = models.CharField(max_length=50, blank=False,unique=True)
+    phone_number = models.CharField(max_length=22)
+    location = models.CharField(max_length=60)
+    password = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return f'{self.user_name} {self.phone_number} {self.location}'
+
+
