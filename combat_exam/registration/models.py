0e89c393a8aa25526d787a1d5933e40a475c61b6
mahdi <mahdi.mohseni0000@gmail.com>
2024-08-25 21:18:51 +0330

    befor view made

--- combat_exam/registration/models.py
+++ combat_exam/registration/models.py
@@ -7,9 +7,9 @@ class User (models.Model):
     user_name = models.CharField(max_length=50, blank=False,unique=True)
     phone_number = models.CharField(max_length=22)
     location = models.CharField(max_length=60)
-    password = models.CharField(max_length=30)
+    password = models.CharField(max_length=30, blank=False)
     
     def __str__(self):
-        return f'{self.user_name} {self.phone_number} {self.location}'
+        return f'{self.user_name} : {self.phone_number} : {self.location}'
 
 
